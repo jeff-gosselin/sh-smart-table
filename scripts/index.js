c2d16884bcdcc3a639a65f1e5cf2149c98c3d8f8
jeff-gosselin <jgoz.email@gmail.com>
2019-08-16 13:03:55 -0400

    adding functionality to the sort and filter buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,25 @@
-import tableData from './tableData.js';
-
+import tableData from './tableData.js'; // Imports the data for the table
 
 // Take imported data and create a table
-const renderTable = (data) => {
+renderTable(tableData);
+
+// Add eventListeners to all column header sort and filter icons
+addEventListenersForSortAndFilter();
+
+function addEventListenersForSortAndFilter() {
+    const icons = document.querySelectorAll('i');
+
+    for (let icon of icons) {
+        if (icon.innerText === 'swap_vert') {
+            icon.addEventListener('click', () => sortColumn(icon.dataset.id));
+        } else {
+            icon.addEventListener('click', () => filterColumn(icon.dataset.id));
+        }
+    }
+}
+
+
+function renderTable(data) {
     const thead = document.querySelector('#main-container thead');
     const tbody = document.querySelector('#main-container tbody');
     const trHead = document.createElement('tr');
@@ -12,16 +29,13 @@ const renderTable = (data) => {
     const colHeaders = Object.keys(data[0]);
     for (let i = 0; i < colHeaders.length; i++) {
         trHead.innerHTML += `
-            <th data-id="header-${i+1}">
-                <div class="options">
-                    
-                </div>
-
+            <th data-id="${colHeaders[i]}">
+                <div class="options"></div>
                 <div>
                     <span class="col-header">${colHeaders[i]}</span>
                     <div>
-                        <i style="display: none" data-id="header-${i+1}" class="material-icons">swap_vert</i>
-                        <i style="display: none" data-id="header-${i+1}" class="material-icons">filter_list</i>
+                        <i style="display: none" data-id="${colHeaders[i]}" class="material-icons">swap_vert</i>
+                        <i style="display: none" data-id="${colHeaders[i]}" class="material-icons">filter_list</i>
                     </div>
                 </div>
             </th>`;
@@ -43,7 +57,7 @@ const renderTable = (data) => {
 
 }
 
-renderTable(tableData);
+
 
 function showOptions(id) { 
     const options = document.querySelector(`[data-id="${id}"] .options`);
@@ -59,7 +73,6 @@ function showOptions(id) {
     let btns = options.querySelectorAll('button');
     btns[0].addEventListener('click', (e) => toggleSort(e));
     btns[1].addEventListener('click', (e) => toggleFilter(e));
-    // console.log(btns[0]);
 }
 
 function toggleSort(e) {
@@ -78,4 +91,17 @@ function toggleFilter(e) {
     } else {
         filterBtn.style.display = 'none';
     }
+}
+
+function sortColumn(columnName) {
+    // find out the type of data in the column to choose a sort method
+    let typeOfData = typeof tableData[0][columnName];
+    console.log(typeOfData);
+
+    // switch the direction of the sort
+    
+}
+
+function filterColumn(columnName) {
+    console.log("filtered!", columnName);
 }
